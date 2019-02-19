67cf40b2f2d45f897227e0dc05dd8e111941cab0
Luke Halley <luke123halley@gmail.com>
2019-02-19 17:25:42 +1000

    Implement offline caching strategy

--- src/decode/decode_offline.py
+++ src/decode/decode_offline.py
@@ -6,6 +6,7 @@
     
     This module provides decoding capabilities without external API calls.
     """
+# TODO: Add local cache for frequently used function signatures to reduce API calls
 """Decode function calls without external API dependencies.
 
     Provides fallback decoding using locally cached ABI definitions.
