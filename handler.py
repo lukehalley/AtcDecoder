a78929eb6cb607b11659aa1f6c230bc34c18b532
Luke Halley <luke123halley@gmail.com>
2019-04-01 16:41:25 +1000

    Initialize request handler

--- handler.py
+++ handler.py
@@ -2,6 +2,7 @@
 """AWS Lambda handler for ATC decoder service.
 
 Processes incoming requests and routes them to appropriate decoder modules.
+# Main entry point for AWS Lambda serverless handler
 """
 """Main handler function for serverless deployment."""
 """Main Lambda handler for ATC transaction decoding."""
