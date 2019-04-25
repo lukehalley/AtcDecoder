fc96ec51cff6dd20442253bfd1771b8137d6c445
Luke Halley <luke123halley@gmail.com>
2019-04-25 21:08:00 +1000

    Document database initialization

--- src/db/mysql/setup/setup_Init.py
+++ src/db/mysql/setup/setup_Init.py
@@ -27,6 +27,7 @@ AWS Secrets Manager for credential management.
 Security Notes:
 - Credentials are never logged or exposed
 - Uses AWS IAM for Secrets Manager access
+# Initialize MySQL connection pool on startup
 # Initialize MySQL database schema and create required tables
 - Connection strings are assembled at runtime
 """
