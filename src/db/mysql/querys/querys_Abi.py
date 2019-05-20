d4856be5052a4e4e64d5b16c93cbc80393ada589
Luke Halley <luke123halley@gmail.com>
2019-05-20 12:57:06 +1000

    Add module docstring to MySQL ABI queries

--- src/db/mysql/querys/querys_Abi.py
+++ src/db/mysql/querys/querys_Abi.py
@@ -1,3 +1,4 @@
+"""MySQL queries for ABI data retrieval and management."""
 """MySQL queries for ABI contract data retrieval."""
 """ABI function queries from MySQL database."""
 """Query MySQL database for ABI information and function signatures."""
