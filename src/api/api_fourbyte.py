dcca31e218b9f575506ddf024e2b5a95901e3689
Luke Halley <luke123halley@gmail.com>
2019-05-30 17:41:56 +1000

    Document 4Byte API integration

--- src/api/api_fourbyte.py
+++ src/api/api_fourbyte.py
@@ -1,5 +1,6 @@
 """Integration with 4byte database for function signature lookup.
 
+# Integration with 4Byte Directory API for function signature resolution
 Provides caching and fallback mechanisms for ABI resolution.
 """
 """Fourbyte database API integration for function signatures."""
