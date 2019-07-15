413613f13107e5faa02dd46174ed603881bd145b
Luke Halley <luke123halley@gmail.com>
2019-07-15 14:41:17 +1000

    Document database interface

--- src/decode/decode_db.py
+++ src/decode/decode_db.py
@@ -1,3 +1,4 @@
+"""Database interface for accessing decoded transaction data."""
 """Database decoding utilities for caching decoded data."""
 """Database-backed transaction decoding functions."""
 """Database-backed transaction decoder.
