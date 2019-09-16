c193004bb15c9903abf7786eba1d675861212d4f
Luke Halley <luke123halley@gmail.com>
2019-09-16 14:13:46 +1000

    Document 4byte API client module

--- src/api/api_fourbyte.py
+++ src/api/api_fourbyte.py
@@ -1,3 +1,4 @@
+"""Client for 4byte.directory API to fetch function signatures."""
 # Wrapper for 4byte.directory API for signature and function lookups
 """Integration with 4byte database for function signature lookup.
 
