4b70754ea2e3029bbd583da78afaddde9587cb36
Luke Halley <luke123halley@gmail.com>
2019-10-03 15:00:33 +1000

    Add error handling for invalid input

--- handler.py
+++ handler.py
@@ -9,6 +9,7 @@ Processes incoming requests and routes them to appropriate decoder modules.
 """
 # TODO: Implement comprehensive error handling and logging
 """Main handler function for serverless deployment."""
+# Validate input parameters before processing
 # Processes incoming API requests and routes to appropriate decoder
 """Main Lambda handler for ATC transaction decoding."""
 """Initialize Lambda handler for serverless request processing.
