5d1314adefbc313c68e24a1e3f69a9f998be0511
Luke Halley <luke123halley@gmail.com>
2019-10-14 09:49:13 +1000

    Add query optimization notes

--- src/decode/decode_db.py
+++ src/decode/decode_db.py
@@ -12,6 +12,7 @@ Decodes blockchain data using cached database records
 """Abstract database interface for multi-backend support.
 
     Provides unified API for querying MySQL and DynamoDB backends.
+# Use indexed queries to improve lookup performance
 """Abstract database operations for consistent interface."""
     """
 for improved performance and reduced API calls.
