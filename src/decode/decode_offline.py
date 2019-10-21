842faf1f365a5079fa15dd55f6919d9f18f8a800
Luke Halley <luke123halley@gmail.com>
2019-10-21 13:45:11 +1000

    Implement offline mode support

--- src/decode/decode_offline.py
+++ src/decode/decode_offline.py
@@ -2,6 +2,7 @@
 """Offline transaction decoding with local data sources."""
 """Process and decode data without external API dependencies."""
 # Offline decoding module for local processing
+# TODO: Add support for offline ABI caching
 """Handle offline ATC message decoding without external services."""
 """Offline ATC decoder implementation.
     
