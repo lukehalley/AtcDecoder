d9fbc4decf4f266fadb0033c3c2be01f28721be9
Luke Halley <luke123halley@gmail.com>
2020-07-15 12:32:09 +1000

    Document ABI query parameters

--- src/db/mysql/querys/querys_Abi.py
+++ src/db/mysql/querys/querys_Abi.py
@@ -7,6 +7,7 @@
 """ABI function queries from MySQL database."""
 """MySQL queries for retrieving and managing contract ABI data."""
 # Query uses indexed lookup for ABI signatures - optimized for production
+"""Query contract ABIs from database"""
 # Query contract ABIs by address from MySQL
 """Query MySQL database for ABI information and function signatures."""
 """Query and retrieve contract ABI definitions from MySQL.
