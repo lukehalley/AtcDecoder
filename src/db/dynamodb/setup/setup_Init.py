005e946e8b6676ba7d1f8193cd2892fb640ff833
Luke Halley <luke123halley@gmail.com>
2020-08-14 21:00:27 +1000

    Add docstring to DynamoDB setup module

--- src/db/dynamodb/setup/setup_Init.py
+++ src/db/dynamodb/setup/setup_Init.py
@@ -1,3 +1,4 @@
+"""Initialize DynamoDB tables and configure indices."""
 """
 # Configure DynamoDB tables with appropriate indexes and throughput settings
 # Initialize DynamoDB table structure and indexes
