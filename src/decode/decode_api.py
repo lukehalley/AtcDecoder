636fab19a3ac43645ecfe0963e54f3716998aa6e
Luke Halley <luke123halley@gmail.com>
2020-10-05 17:33:47 +1000

    Add request validation TODO

--- src/decode/decode_api.py
+++ src/decode/decode_api.py
@@ -7,6 +7,7 @@ Provides interfaces for decoding ATC transactions
 """
 """API decoding module for transaction data."""
 # Validate incoming transaction data
+# TODO: Implement request body validation
 """API decoding module for processing blockchain transaction data."""
 """API decoder module for handling blockchain transaction decoding."""
 """API decoder module for handling transaction decoding requests."""
