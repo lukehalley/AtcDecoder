96d82514b74030fa2786c5507b3724387b749380
Luke Halley <luke123halley@gmail.com>
2021-01-11 17:27:39 +1000

    Document DynamoDB initialization

--- src/db/dynamodb/setup/setup_Init.py
+++ src/db/dynamodb/setup/setup_Init.py
@@ -1,3 +1,4 @@
+"""Initialize DynamoDB tables and configure indexes for optimal query performance."""
 """Initialize DynamoDB tables and configuration."""
 """Initialize DynamoDB tables and configure indices."""
 """
