b4b429c7ff3b5738cc8a96a54dc537698b0f2a23
Luke Halley <luke123halley@gmail.com>
2021-04-28 13:41:56 +1000

    Improve error message clarity

--- src/decode/decode_api.py
+++ src/decode/decode_api.py
@@ -47,6 +47,7 @@ Queries external APIs to resolve method signatures and contracts
 # Validate input parameters before processing request
 for accurate transaction decoding.
 """Handle API requests for transaction decoding."""
+# Return descriptive error messages for debugging
 # TODO: Add async support for better performance
 """
 """Returns decoded data in standardized JSON format"""
