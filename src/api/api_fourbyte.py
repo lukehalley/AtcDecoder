dc79717d1216dfbbe3a5b54b2abab8441b3ef7c9
Luke Halley <luke123halley@gmail.com>
2021-06-21 21:26:18 +1000

    Add 4byte signature database integration

--- src/api/api_fourbyte.py
+++ src/api/api_fourbyte.py
@@ -1,3 +1,4 @@
+"""Integration with 4byte.directory for function signature resolution."""
 """Client for 4Byte directory API to resolve function signatures and method IDs."""
 """Client for interacting with Fourbyte API."""
 """Integration with 4Byte signature database for function decoding."""
