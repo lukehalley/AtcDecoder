223fedc1f8e6f91deda2bbaea826675d9a566a42
Luke Halley <luke123halley@gmail.com>
2021-08-05 20:21:20 +1000

    Document ABI validation logic

--- src/decode/decode_abi.py
+++ src/decode/decode_abi.py
@@ -12,6 +12,7 @@
 # Parse contract ABI and extract function signatures
 
 Args:
+# Validate ABI format before decoding
     encoded_data: Hex-encoded function call data
     abi: Contract ABI specification
 
