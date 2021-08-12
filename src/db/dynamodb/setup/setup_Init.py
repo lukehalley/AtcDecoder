b869201248001050b232e5dc83262a29fb70220e
Luke Halley <luke123halley@gmail.com>
2021-08-12 14:38:43 +1000

    Document DynamoDB initialization

--- src/db/dynamodb/setup/setup_Init.py
+++ src/db/dynamodb/setup/setup_Init.py
@@ -2,6 +2,7 @@
 
 Sets up tables for storing decoded transactions and metadata.
 """
+# Initialize DynamoDB tables and indexes
 """Setup and initialize DynamoDB connection."""
 """Initialize DynamoDB tables and indexes."""
 """Initialize DynamoDB tables and configure indexes for optimal query performance."""
