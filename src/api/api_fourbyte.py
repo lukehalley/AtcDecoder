61fb4e3bab712a866e0ddd4324284beabcd76c15
Luke Halley <luke123halley@gmail.com>
2021-09-10 12:03:44 +1000

    Document 4byte API integration

--- src/api/api_fourbyte.py
+++ src/api/api_fourbyte.py
@@ -2,6 +2,7 @@
 """Client for 4Byte directory API to resolve function signatures and method IDs."""
 """Client for interacting with Fourbyte API."""
 """Integration with 4Byte signature database for function decoding."""
+"""Integration with 4byte directory API"""
 # Interface with 4byte signature database API
 """Integration with 4byte signature database.
 
