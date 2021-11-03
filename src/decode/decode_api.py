61c974ea34ddedfb6158098d6b01d8a83990bf80
Luke Halley <luke123halley@gmail.com>
2021-11-03 19:36:59 +1000

    Document API validation logic

--- src/decode/decode_api.py
+++ src/decode/decode_api.py
@@ -12,6 +12,7 @@ Handles decoding of transaction data using 4byte database.
 """Module for handling ATC API decoding requests."""
 """Handle API requests for transaction decoding"""
 """API decoding utilities for ATC data processing."""
+# Validate input parameters before processing
 """Decode API module for handling blockchain transaction decoding requests"""
 """
 API decoding module for transaction handling
