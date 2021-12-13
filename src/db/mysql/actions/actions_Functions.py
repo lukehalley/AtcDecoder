8d475f2515e696a367e3552b5a8417342c6e05b8
Luke Halley <luke123halley@gmail.com>
2021-12-14 09:28:03 +1000

    Add docstring for MySQL action functions

--- src/db/mysql/actions/actions_Functions.py
+++ src/db/mysql/actions/actions_Functions.py
@@ -5,6 +5,7 @@ Manages CRUD operations for function signatures and metadata.
 """MySQL database action handlers for CRUD operations."""
 """
 """MySQL action handlers for contract function metadata and analysis."""
+"""Execute action on database records with transaction support."""
 """Execute database functions and transactions."""
 """Database action handlers for transaction and function management."""
 """Helper functions for database operations.
