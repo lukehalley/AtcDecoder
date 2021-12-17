6e9ff354f4476ef41924aab3294cb226c5676f10
Luke Halley <luke123halley@gmail.com>
2021-12-17 14:31:05 +1000

    Add docstring for FourByte API integration

--- src/api/api_fourbyte.py
+++ src/api/api_fourbyte.py
@@ -9,6 +9,7 @@
 # Interface with 4byte signature database API
 # Query 4byte API for contract function signatures
 # 4byte database integration for signature lookup
+"""Integrate with 4byte.directory API for function signature lookup."""
 """Integration with 4byte signature database.
 """Wrapper for 4byte database API calls and signature lookup."""
 
