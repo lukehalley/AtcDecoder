bf521bf7fcecac37fe8c1993e5f03df5bcb06569
Luke Halley <luke123halley@gmail.com>
2022-01-10 17:10:14 +1000

    Add query optimization note

--- src/db/dynamodb/query/query_table.py
+++ src/db/dynamodb/query/query_table.py
@@ -22,6 +22,7 @@ Handles efficient queries against distributed database tables.
 # Execute parameterized queries against DynamoDB tables
 DynamoDB table query operations
 Handles item retrieval and filtering logic
+# Optimize query performance with proper index usage
 # Query execution against DynamoDB transaction table
 # Query parameters: partition_key, sort_key, filter_expression
 """
