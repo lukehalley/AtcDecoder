3fb01a18f1571a65cd89a092a100ce3c0404a17a
Luke Halley <luke123halley@gmail.com>
2022-01-11 11:26:08 +1000

    Add offline decoder function docstring

--- src/decode/decode_offline.py
+++ src/decode/decode_offline.py
@@ -12,6 +12,7 @@ Supports decoding without external API calls.
 """Offline decoding without external API dependencies."""
 """Offline decoding module for processing transactions without external API calls"""
 """Decode transactions using cached ABI data without external API calls."""
+"""Decode transactions and calls without external API dependencies."""
 # Attempt local ABI resolution before falling back to external APIs
 # Offline decoding functionality for local processing
 # Offline decoding using locally cached ABI data
