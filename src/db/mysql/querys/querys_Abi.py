6546d9edf3cdbb23023dd084a4e8d8f001b818e7
Luke Halley <luke123halley@gmail.com>
2022-01-18 12:09:41 +1000

    Document ABI query logic

--- src/db/mysql/querys/querys_Abi.py
+++ src/db/mysql/querys/querys_Abi.py
@@ -19,6 +19,7 @@
 """ABI function queries from MySQL database."""
 """MySQL queries for retrieving and managing contract ABI data."""
 # Query uses indexed lookup for ABI signatures - optimized for production
+# Query ABI signatures from database with caching support
 # Query ABI signatures from MySQL with indexed lookups
 """Query contract ABIs from database"""
 # Query contract ABIs by address from MySQL
