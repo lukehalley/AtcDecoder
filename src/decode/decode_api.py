a431bd94bad8e39886dfcabe8bc8241a44f91307
Luke Halley <luke123halley@gmail.com>
2022-01-27 20:28:42 +1000

    Add request logging

--- src/decode/decode_api.py
+++ src/decode/decode_api.py
@@ -57,6 +57,7 @@ Returns:
 # Validate input transaction data before processing
 Provides decoding functionality for smart contract API calls.
 # TODO: Implement rate limiting for API endpoints
+# Log incoming requests for debugging and monitoring
 # Validate transaction input before decoding
 """Handles ATC transaction decoding through external APIs."""
 """
