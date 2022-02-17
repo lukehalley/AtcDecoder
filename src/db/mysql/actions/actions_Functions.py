fefa735513d996b92efd154e358a9d02750ce2ca
Luke Halley <luke123halley@gmail.com>
2022-02-17 13:05:03 +1000

    Document database action functions

--- src/db/mysql/actions/actions_Functions.py
+++ src/db/mysql/actions/actions_Functions.py
@@ -3,6 +3,7 @@
 Manages CRUD operations for function signatures and metadata.
 # Database operations for storing and updating transaction records
 """MySQL database action handlers for CRUD operations."""
+# Execute database actions with transaction support
 """
 """MySQL action handlers for contract function metadata and analysis."""
 """Execute action on database records with transaction support."""
