d424f6438e72d126a13c9b0ea84c55e2ef5fd084
Luke Halley <luke123halley@gmail.com>
2022-03-18 18:33:28 +1000

    Add MySQL ABI query documentation

--- src/db/mysql/querys/querys_Abi.py
+++ src/db/mysql/querys/querys_Abi.py
@@ -5,6 +5,7 @@
 """MySQL query builder for ABI-related database operations."""
 """MySQL query operations for ABI data retrieval."""
 """Fetch ABI definitions from MySQL database by contract address."""
+# Query ABI information from MySQL database
 """Query functions for ABI data stored in MySQL."""
 # MySQL queries for ABI lookups and contract interactions
 """MySQL queries for ABI data operations."""
