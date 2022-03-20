cd4a118a4f16db13cdd3c0bd28d7b50c634e49a2
Luke Halley <luke123halley@gmail.com>
2022-03-21 00:07:16 +0800

    added offline decode

--- handler.py
+++ handler.py
@@ -1,7 +1,9 @@
 from web3 import Web3
 
 from src.db.querys.querys_Abi import getAbiByDbId
-from src.decode.decode import decode_tx
+from src.decode.decode_abi import decode_tx
+from src.decode.decode_offline import OfflineDecode
+
 
 def invoke(event, context):
 
@@ -21,16 +23,19 @@ def invoke(event, context):
     # Get Transaction
     TransactionDetails = web3Instance.eth.get_transaction(TxHash)
 
-    # Invoke Decode
-    DecodeSuccessful, DecodeMsg, FunctionName, FunctionParams, TargetSchema = decode_tx(ContractHash, TransactionDetails["input"], Abi)
+    # Try Offline Decode
+    DecodeSuccessful, DecodeMsg, FunctionName, FunctionParams = OfflineDecode(TransactionDetails["input"])
+
+    # If That Fails Try ABI Decode
+    if not DecodeSuccessful:
+        DecodeSuccessful, DecodeMsg, FunctionName, FunctionParams = decode_tx(ContractHash, TransactionDetails["input"], Abi)
 
     # Collect Results
     if DecodeSuccessful:
 
         DecodeBody = {
             "function": FunctionName,
-            "args": FunctionParams,
-            "schema": TargetSchema
+            "args": FunctionParams
         }
 
         ReturnBody = {"statusCode": 200, "msg": DecodeMsg, "body": DecodeBody}
@@ -42,11 +47,20 @@ def invoke(event, context):
 
     return ReturnBody
 
+# GOOD
 # args = {
 #   "rpc_url": "https://eth.llamarpc.com",
 #   "tx_hash": "0xe8f26d91a2a8af6747670d8fde7ccf188539582d03e08441ce0228d057c0ac70",
 #   "contract_hash": "0x6131B5fae19EA4f9D964eAc0408E4408b66337b5",
 #   "contract_abi_db_id": 362
 # }
-#
-# invoke(event=args, context="")
+
+# BAD
+args = {
+  "rpc_url": "https://eth.llamarpc.com",
+  "tx_hash": "0x28150100bd1a9217b2edd74222fb3cc01ec0134a898601005e78b51d74b9e6b7",
+  "contract_hash": "0x84D99Aa569D93a9CA187D83734c8C4a519c4e9b1",
+  "contract_abi_db_id": 394
+}
+
+invoke(event=args, context="")
