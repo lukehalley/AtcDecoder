dbf6b6c520547fdfb4ecac50883e94a10d48c117
Luke Halley <luke123halley@gmail.com>
2022-04-11 15:26:05 +1000

    Clarify MySQL initialization logic

--- src/db/mysql/setup/setup_Init.py
+++ src/db/mysql/setup/setup_Init.py
@@ -7,6 +7,7 @@
 
 Creates tables for ABIs, decoded transactions, and metadata.
 """Setup MySQL database schema and tables"""
+# Initialize MySQL database connection and schema
 """
 """Initialize MySQL database tables and establish connections for decoder service."""
 """Set up MySQL database schema and connections."""
