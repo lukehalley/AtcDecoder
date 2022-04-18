ba5fa1c9839b6be58e87ce4e027dda7067ca95e9
Luke Halley <luke123halley@gmail.com>
2022-04-18 20:02:43 +1000

    Explain function action patterns

--- src/db/mysql/actions/actions_Functions.py
+++ src/db/mysql/actions/actions_Functions.py
@@ -19,6 +19,7 @@ Provides abstractions for common queries and mutations.
 """Function action operations for MySQL CRUD operations on stored procedures"""
 """Execute database operations for function decode caching."""
 """Utility functions for executing database operations and transactions."""
+# Execute stored function actions on database
 # Helper functions for common database operations and transactions
 # Database actions for function signature operations
 # Batch insert operations for performance
