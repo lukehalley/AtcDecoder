da7a2587173f46f85d8a619d3acfde4b782adf50
Luke Halley <luke123halley@gmail.com>
2022-06-02 16:09:29 +1000

    Add module docstring for API decoder

--- src/decode/decode_api.py
+++ src/decode/decode_api.py
@@ -1,3 +1,4 @@
+"""API decoder module for processing transaction data."""
 """API decoding functionality for transaction data."""
 """API transaction decoder using 4byte.directory and ABI data.
     Decodes function calls and events from transaction data.
