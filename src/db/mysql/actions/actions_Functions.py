a81691c917339a714778f2fa6b6682c2ab9559f8
Luke Halley <luke123halley@gmail.com>
2022-06-17 12:55:06 +1000

    Add clarification for function action handlers

--- src/db/mysql/actions/actions_Functions.py
+++ src/db/mysql/actions/actions_Functions.py
@@ -5,6 +5,7 @@ Manages CRUD operations for function signatures and metadata.
 # Database operations for storing and updating transaction records
 """MySQL database action handlers for CRUD operations."""
 # Execute database actions with transaction support
+# Execute database actions for function metadata
 """
 """MySQL action handlers for contract function metadata and analysis."""
 """Execute action on database records with transaction support."""
