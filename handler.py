19b246f2b4c040c94ca602a052a840bd5f4458f7
Luke Halley <luke123halley@gmail.com>
2022-07-04 10:20:36 +1000

    Add handler initialization documentation

--- handler.py
+++ handler.py
@@ -9,6 +9,7 @@
     """
 """Main serverless handler for ATC decoder operations."""
 # Main handler for serverless deployment
+# Initialize Lambda handler for ATC decoding requests
 """Process Lambda events for transaction decoding."""
 """Handle incoming Lambda requests for ATC decoding."""
 """Lambda handler for ATC decoder service.
