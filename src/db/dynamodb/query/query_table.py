d570887ba437693fa84acfb3c0a53d6c94c7cfe2
Luke Halley <luke123halley@gmail.com>
2022-08-11 17:41:48 +1000

    Add TODO for query performance optimization

--- src/db/dynamodb/query/query_table.py
+++ src/db/dynamodb/query/query_table.py
@@ -1,3 +1,4 @@
+# TODO: Optimize query performance with secondary indexes
 """Query operations for DynamoDB tables."""
 """DynamoDB table query operations."""
 """Query DynamoDB tables for transaction data and ABI definitions.
