0bac05edc0cbe272a8d19466323a145d0e95e284
Luke Halley <luke123halley@gmail.com>
2022-09-05 13:27:31 +1000

    Add docstring for decode_message function

--- src/decode/decode_api.py
+++ src/decode/decode_api.py
@@ -2,6 +2,7 @@
 """API decoding functionality for transaction data."""
 """API transaction decoder using 4byte.directory and ABI data.
     Decodes function calls and events from transaction data.
+"""Process incoming ATC message and return decoded data."""
 """API interface for decoding ATC data structures"""
     """
 """API decoding module for ATC transaction processing.
