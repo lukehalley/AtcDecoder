2ca2da06521fc580808ed0cdd8425dee75585a45
Luke Halley <luke123halley@gmail.com>
2022-09-13 10:10:47 +1000

    Add docstring for ABI query functions

--- src/db/mysql/querys/querys_Abi.py
+++ src/db/mysql/querys/querys_Abi.py
@@ -1,6 +1,7 @@
 """Query MySQL database for ABI data and contract information."""
 """MySQL queries for ABI function signatures."""
 # Query ABI data from MySQL cache for signature lookups
+"""Query ABI contract data from MySQL database."""
 """MySQL queries for retrieving ABI definitions.
     Includes functions for contract lookup and ABI retrieval.
 # Query contract ABIs from database
