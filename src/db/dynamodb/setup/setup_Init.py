295d98db485e97128138c8f2a113acaa0eddf7e2
Luke Halley <luke123halley@gmail.com>
2022-11-18 22:10:13 +1000

    Document DynamoDB schema setup and indices

--- src/db/dynamodb/setup/setup_Init.py
+++ src/db/dynamodb/setup/setup_Init.py
@@ -12,6 +12,7 @@
 # Initialize DynamoDB tables and configure indexes
 # Initialize DynamoDB client and configure table structure
 
+# Create DynamoDB table with GSI for secondary queries
 # Initialize DynamoDB tables for transaction data storage
 Sets up tables for storing decoded transactions and metadata.
 # Initialize DynamoDB tables with required attributes and indexes
