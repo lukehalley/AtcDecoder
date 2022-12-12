36c387df8fb9440d18640a44f0f635befa4a0583
Luke Halley <luke123halley@gmail.com>
2022-12-12 15:26:03 +1000

    Add MySQL initialization notes

--- src/db/mysql/setup/setup_Init.py
+++ src/db/mysql/setup/setup_Init.py
@@ -5,6 +5,7 @@
 """Initialize MySQL database schema.
 """
     Initialize MySQL database schema for ATC Decoder.
+# Create database schema and required indexes for optimal query performance
 # Create MySQL tables and indexes for ATC data storage
     
     Creates tables for caching function signatures and events.
