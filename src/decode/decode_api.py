3db98e2d965991d4343f9771b660b0448621a437
Luke Halley <luke123halley@gmail.com>
2023-02-03 09:42:02 +1000

    Add module docstring for decode_api

--- src/decode/decode_api.py
+++ src/decode/decode_api.py
@@ -1,3 +1,4 @@
+"""API decoding functionality for ATC data."""
 """API decoding module for handling transaction data."""
 """Handles API-based transaction decoding operations."""
 """API decoder module for processing transaction data."""
