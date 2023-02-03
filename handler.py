f0c4dab158b8db8be96e54f415bff51a1aba73cd
Luke Halley <luke@Lukes-MacBook-Pro.local>
2023-02-03 08:53:43 +0000

    using dynamodb

--- handler.py
+++ handler.py
@@ -1,8 +1,9 @@
 from web3 import Web3
 
-from src.db.querys.querys_Abi import getAbiByDbId
+from src.db.mysql.querys.querys_Abi import getAbiByDbId
 from src.decode.decode_abi import decode_tx
 from src.decode.decode_offline import OfflineDecode
+from src.decode.decode_online import OnlineDecode
 
 
 def invoke(event, context):
@@ -13,21 +14,23 @@ def invoke(event, context):
     ContractHash = event["contract_hash"]
     ContractAbiDbId = int(event["contract_abi_db_id"])
 
-    # Get ABI From DB
-    AbiFromDB = getAbiByDbId(ContractAbiDbId)
-    Abi = AbiFromDB["abi"]
-
     # Connect To RPC
     web3Instance = Web3(Web3.HTTPProvider(RPCUrl))
 
     # Get Transaction
     TransactionDetails = web3Instance.eth.get_transaction(TxHash)
 
+    # Try Decode With API
+    DecodeSuccessful, DecodeMsg, FunctionName, FunctionParams = OnlineDecode(TransactionDetails["input"])
+
     # Try Offline Decode
     DecodeSuccessful, DecodeMsg, FunctionName, FunctionParams = OfflineDecode(TransactionDetails["input"])
 
     # If That Fails Try ABI Decode
     if not DecodeSuccessful:
+        # Get ABI From DB
+        AbiFromDB = getAbiByDbId(ContractAbiDbId)
+        Abi = AbiFromDB["abi"]
         DecodeSuccessful, DecodeMsg, FunctionName, FunctionParams = decode_tx(ContractHash, TransactionDetails["input"], Abi)
 
     # Collect Results
@@ -47,20 +50,30 @@ def invoke(event, context):
 
     return ReturnBody
 
-# GOOD
-# args = {
-#   "rpc_url": "https://eth.llamarpc.com",
-#   "tx_hash": "0xe8f26d91a2a8af6747670d8fde7ccf188539582d03e08441ce0228d057c0ac70",
-#   "contract_hash": "0x6131B5fae19EA4f9D964eAc0408E4408b66337b5",
-#   "contract_abi_db_id": 362
-# }
 
-# BAD
-args = {
-  "rpc_url": "https://eth.llamarpc.com",
-  "tx_hash": "0x28150100bd1a9217b2edd74222fb3cc01ec0134a898601005e78b51d74b9e6b7",
-  "contract_hash": "0x84D99Aa569D93a9CA187D83734c8C4a519c4e9b1",
-  "contract_abi_db_id": 394
+
+# AVAX
+avax_args = {
+  "rpc_url": "https://api.avax.network/ext/bc/C/rpc",
+  "tx_hash": "0x1d5a74894c85263b5ccacb48907b2d1b6f425a4cb77fa303006214e762f9ac45",
+  "contract_hash": "0xE3Ffc583dC176575eEA7FD9dF2A7c65F7E23f4C3",
+  "contract_abi_db_id": 417
 }
 
-invoke(event=args, context="")
+# ETH
+eth_args = {
+  "rpc_url": "https://eth.llamarpc.com",
+  "tx_hash": "0xe8f26d91a2a8af6747670d8fde7ccf188539582d03e08441ce0228d057c0ac70",
+  "contract_hash": "0x6131B5fae19EA4f9D964eAc0408E4408b66337b5",
+  "contract_abi_db_id": 362
+}
+
+# BSC
+bsc_args = {
+  "rpc_url": "https://bsc-dataseed.binance.org",
+  "tx_hash": "0xa0a60d94026e7b65dcae9bf1addcb85586e358d0fa6f445d87b3d5f3a1953774",
+  "contract_hash": "0x10ed43c718714eb63d5aa57b78b54704e256024e",
+  "contract_abi_db_id": 417
+}
+
+invoke(event=bsc_args, context="")
