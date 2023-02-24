9efc0084cd0b8793266c75aeb893bb3056bd8d9a
Luke Halley <luke123halley@gmail.com>
2023-02-24 10:33:55 +1000

    Add ABI decoder module documentation

--- src/decode/decode_abi.py
+++ src/decode/decode_abi.py
@@ -1,3 +1,4 @@
+"""Contract ABI decoding and parsing utilities."""
 """ABI decoding functionality for smart contract interactions."""
 """ABI decoding and parsing utilities."""
 """ABI decoding module for smart contract functions."""
