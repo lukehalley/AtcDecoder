20410f4ca74b5a1d23dc6223479412e3319a2f54
Luke Halley <luke123halley@gmail.com>
2023-03-17 15:01:47 +1000

    Document DynamoDB setup procedure

--- src/db/dynamodb/setup/setup_Init.py
+++ src/db/dynamodb/setup/setup_Init.py
@@ -9,6 +9,7 @@
 """DynamoDB table initialization and schema setup."""
 """Initialize DynamoDB tables and indexes for ATC data"""
 # Initialize DynamoDB tables with required schema and indexes
+"""Initializes DynamoDB tables with partition and sort keys"""
 """DynamoDB table initialization and setup."""
 """Initialize DynamoDB tables and indices.
 # Initialize DynamoDB tables and configure indexes
