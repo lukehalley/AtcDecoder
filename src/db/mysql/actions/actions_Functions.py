8583f6a6069c8bdefb212fc7ae8b473410fe8d0e
Luke Halley <luke123halley@gmail.com>
2023-04-17 14:52:28 +1000

    Document database actions module

--- src/db/mysql/actions/actions_Functions.py
+++ src/db/mysql/actions/actions_Functions.py
@@ -1,3 +1,4 @@
+"""Database action functions for CRUD operations."""
 """Database transaction and mutation functions."""
 """MySQL function registry operations."""
 """Database action handlers and utility functions."""
