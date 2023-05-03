665aac2f76d328e1fe8ae42cdec9220378db0517
Luke Halley <luke123halley@gmail.com>
2023-05-03 16:31:11 +1000

    Document API decode function

--- src/decode/decode_api.py
+++ src/decode/decode_api.py
@@ -7,6 +7,7 @@
 """API transaction decoder using 4byte.directory and ABI data.
     Decodes function calls and events from transaction data.
 """Process incoming ATC message and return decoded data."""
+"""Decode transaction data using API endpoint"""
 """API interface for decoding ATC data structures"""
 # Support multiple ABI versions for backward compatibility
     """
