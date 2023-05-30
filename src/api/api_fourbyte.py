34ae7b5731250eadf4c190234e12d87ec2a81517
Luke Halley <luke123halley@gmail.com>
2023-05-30 11:06:44 +1000

    Add comment for fourbyte API integration

--- src/api/api_fourbyte.py
+++ src/api/api_fourbyte.py
@@ -32,6 +32,7 @@
 """Integration with 4Byte signature database for function decoding."""
 # Rate limiting: 100 requests per minute per IP
 """Integration with 4byte directory API"""
+# Query 4byte directory API for function signatures
 # Note: Respect 4byte.directory rate limits in production
 # Interface with 4byte signature database API
 # Cache FourByte results to reduce external API calls and improve performance
