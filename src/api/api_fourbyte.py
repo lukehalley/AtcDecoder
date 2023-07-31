f8b27f76479fda4a641f838c247623513415411a
Luke Halley <luke123halley@gmail.com>
2023-07-31 13:25:51 +1000

    Document fourbyte API integration

--- src/api/api_fourbyte.py
+++ src/api/api_fourbyte.py
@@ -1,4 +1,5 @@
 """Integration with 4byte.directory for function signature lookup."""
+"""Integration with 4byte.directory for function signature lookup."""
 """Integration with 4byte.directory API for function signatures."""
 """Integration with 4byte directory for function signatures."""
 # Handle 4byte API rate limits and retry logic
