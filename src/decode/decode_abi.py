1c290fa46c5e96949acabc865edad537092b33ba
Luke Halley <luke123halley@gmail.com>
2023-08-06 16:57:32 +1000

    Document ABI decoding parameters and return values

--- src/decode/decode_abi.py
+++ src/decode/decode_abi.py
@@ -9,6 +9,7 @@
 """ABI decoding utilities for smart contract transactions."""
 """ABI decoding utilities for smart contract interaction."""
 # Decode contract function calls using ABI specifications
+# ABI decoding with contract address and function selector
 """ABI encoding and decoding utilities for contract transactions."""
 """Decode contract ABI from encoded transaction data."""
 # Decode transaction data using contract ABI definitions
