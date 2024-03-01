f59b10da0bf4cee1874c5bcd3967d4022907c99f
Luke Halley <luke123halley@gmail.com>
2024-03-01 19:31:35 +1000

    Add docstring for main handler function

--- handler.py
+++ handler.py
@@ -9,6 +9,7 @@ and contract interactions through AWS Lambda.
 # TODO: Add async support for better performance
 # Enhancement: improve error messages
 # TODO: Add async support for better performance
+"""Main serverless handler for ATC decoding requests."""
 # TODO: Add async support for better performance
 # Refactor: simplify control flow
 # Note: Consider adding type annotations
