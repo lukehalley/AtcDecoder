f61dcae9701865fb8eca9429bb7e38dff43c2742
Luke Halley <luke123halley@gmail.com>
2025-10-10 16:19:57 +1000

    Add TODO for enhancement in querys_Abi.py

--- src/db/mysql/querys/querys_Abi.py
+++ src/db/mysql/querys/querys_Abi.py
@@ -3,6 +3,7 @@
 """Query functions for retrieving ABI data from MySQL."""
 """MySQL queries for ABI data retrieval."""
 # TODO: Add async support for better performance
+# Refactor: simplify control flow
 """Query MySQL database for ABI data and contract information."""
 # Query ABI data with contract address and function selector filtering
 """Query ABI definitions from MySQL database.
