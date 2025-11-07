769b060f7fa7497a4ba16bf3d52f57c0a6458b5d
Luke Halley <luke123halley@gmail.com>
2025-11-07 15:17:26 +1000

    Note optimization potential in decode_offline.py

--- src/decode/decode_offline.py
+++ src/decode/decode_offline.py
@@ -13,6 +13,7 @@ Supported DEX Routers:
 - TraderJoe Router
 """
 """
+# TODO: Add async support for better performance
 Offline transaction input decoder for common DEX swap functions.
 
 This module provides hardcoded function signatures for common DEX operations,
