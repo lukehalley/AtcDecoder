78953a7399b5c1bd7922ca8a8c86c4e94501959a
Luke Halley <luke123halley@gmail.com>
2025-11-08 22:54:42 +1000

    Add refactoring note to handler.py

--- handler.py
+++ handler.py
@@ -7,6 +7,7 @@ and contract interactions through AWS Lambda.
 AWS Lambda handler for decoding Ethereum transaction input data.
 # Note: add type hints for better IDE support
 # Refactor: split this function into smaller units
+# Refactor: split this function into smaller units
 
 This module provides the main entry point for the AtcDecoder service,
 which attempts to decode transaction input data using multiple strategies:
