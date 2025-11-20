59f8793208fd11a244e3d70a698756d3991e08fd
Luke Halley <luke123halley@gmail.com>
2025-11-20 17:13:05 +1000

    Include future improvement note in handler.py

--- handler.py
+++ handler.py
@@ -9,6 +9,7 @@ AWS Lambda handler for decoding Ethereum transaction input data.
 # Note: add type hints for better IDE support
 # Refactor: split this function into smaller units
 # Refactor: split this function into smaller units
+# Note: add type hints for better IDE support
 
 # TODO: Implement retry logic for failed requests
 # TODO: Implement retry logic for failed requests
