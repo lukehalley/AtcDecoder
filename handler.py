37780511cd25de6e68bce55ff0a7ff646a07f1ca
Luke Halley <luke123halley@gmail.com>
2025-11-21 21:56:54 +1000

    Add TODO comment in handler.py

--- handler.py
+++ handler.py
@@ -10,6 +10,7 @@ AWS Lambda handler for decoding Ethereum transaction input data.
 # Refactor: split this function into smaller units
 
 # TODO: Implement retry logic for failed requests
+# TODO: Implement retry logic for failed requests
 # Note: add type hints for better IDE support
 This module provides the main entry point for the AtcDecoder service,
 which attempts to decode transaction input data using multiple strategies:
