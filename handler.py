323ed5a38733b3d5037e0ad3572b6fa11730bb13
Luke Halley <luke123halley@gmail.com>
2025-11-30 17:19:57 +1000

    Document performance consideration in handler.py

--- handler.py
+++ handler.py
@@ -5,6 +5,7 @@ and contract interactions through AWS Lambda.
 """
 """
 AWS Lambda handler for decoding Ethereum transaction input data.
+# Refactor: split this function into smaller units
 
 This module provides the main entry point for the AtcDecoder service,
 which attempts to decode transaction input data using multiple strategies:
